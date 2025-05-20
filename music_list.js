64811621850a3c218dfa100f6616d082475d4ce3
은진 <ej3133@naver.com>
2025-05-20 15:22:02 +0900

    뮤직플레이어
    평가작업완료

---
+++ music_list.js
@@ -0,0 +1,8 @@
+const musicList = [
+    {name:"Alone", artist:"Color Out", Audio:"Alone", img:"album1", total:"04:05"},
+    {name:"Mahidevran - Maze of sorrow", artist:"Mahidevran rock band", Audio:"Mahidevran-Maze_of_sorrow", img:"album2", total:"06:28"},
+    {name:"No Rest Or Endless Rest", artist:"Lisofv", Audio:"No_Rest_Or_Endless_Rest", img:"album3", total:"04:33"},
+    {name:"Old Melody", artist:"TAB", Audio:"Old_Money", img:"album4", total:"04:01"},
+    {name:"Tantalizing Youth", artist:"Social Square", Audio:"Tantalizing_Youth", img:"album5", total:"04:12"},
+    {name:"The Deep", artist:" Anitek", Audio:"The_Deep", img:"album6", total:"02:24"}
+]
